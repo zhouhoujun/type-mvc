145fcf5d2248c34929923db1787c828ab67b9f52
zhouhoujun <freebirds8281@hotmail.com>
2018-01-16 20:10:45 +0800

    update

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,6 +2,5 @@ export * from './defer';
 
 export * from './types';
 import * as allsymbol from './symbols';
-// export const symbols = allsymbol;
-export const MvcSymbols = allsymbol;
+export const symbols = allsymbol;
 export const mvcSymbols = allsymbol;
