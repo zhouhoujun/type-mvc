e003ec08cf3b7b8236c4b553e6a7c35ed98885ca
houjun <freebirds8282@hotmail.com>
2019-01-28 16:20:40 +0800

    update

--- packages/koa/src/MvcConfigureRegister.ts
+++ packages/koa/src/MvcConfigureRegister.ts
@@ -1,6 +1,6 @@
 import { ConfigureRegister, IRunnableBuilder } from '@ts-ioc/bootstrap';
 import { DebugLogAspect } from '@ts-ioc/logs';
-import { Singleton, IContainer } from '@ts-ioc/core';
+import { Singleton } from '@ts-ioc/core';
 import { IConfiguration } from '@mvx/mvc';
 
 @Singleton
@@ -8,23 +8,22 @@ export class MvcConfigureRegister extends ConfigureRegister<IConfiguration> {
     constructor() {
         super();
     }
-    async register(config: IConfiguration, container: IContainer, builder?: IRunnableBuilder<any>): Promise<void> {
+    async register(config: IConfiguration, builder?: IRunnableBuilder<any>): Promise<void> {
         if (config.debug) {
-            container.register(DebugLogAspect);
+            this.container.register(DebugLogAspect);
         }
         // console.log(builder.getPools().values().length);
         // let topContainer = builder.getPools().values().find(c => lang.getClass(c.getBuilder()) !== ContainerBuilder);
         // lang.assert(topContainer, 'not set run env. use @ts-ioc/platform-server or @ts-ioc/platform-brow(ser');
         // console.log(container.parent);
-        console.log(config, container);
         if (config.controllers) {
-            await container.loadModule({ files: config.controllers });
+            await this.container.loadModule({ files: config.controllers });
         }
         if (config.middlewares) {
-            await container.loadModule({ files: config.middlewares });
+            await this.container.loadModule({ files: config.middlewares });
         }
         if (config.aop) {
-            await container.loadModule({ files: config.aop });
+            await this.container.loadModule({ files: config.aop });
         }
     }
 }
