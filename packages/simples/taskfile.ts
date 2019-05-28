4e4fc11a929df511eaaefc299f82e0adfa8b9afb
houjun <freebirds8281@hotmail.com>
2019-05-29 00:34:52 +0800

    update task

--- packages/simples/taskfile.ts
+++ packages/simples/taskfile.ts
@@ -1,22 +1,41 @@
-import { Pack, PackModule } from '@tsdi/pack';
-import { Workflow } from '@tsdi/activities';
+import { Workflow, Task } from '@tsdi/activities';
+import { PackModule, PackMetadata, LibPackBuilderOption, AssetActivityOption } from '@tsdi/pack';
+import { ServerActivitiesModule } from '@tsdi/platform-server-activities';
+import { AfterInit } from '@tsdi/boot';
 
-@Pack({
+@Task({
+    deps: [
+        PackModule,
+        ServerActivitiesModule
+    ],
     baseURL: __dirname,
-    src: 'src',
-    clean: 'lib',
-    test: 'test/**/*.spec.ts',
-    assets: {
-        ts: { dest: 'lib', annotation: true, uglify: false },
-        html: { dest: 'lib' },
-        js: { dest: 'lib' }
-    }
+    template: [
+        <AssetActivityOption>{
+            activity: 'asset',
+            src: 'src/(views|models)/**/*',
+            dist: '../../dist/simples'
+        },
+        <LibPackBuilderOption>{
+            activity: 'libs',
+            outDir: '../../dist/simples',
+            src: 'src/**/*.ts',
+            test: 'test/**/*.ts',
+            annotation: true,
+            bundles: [
+                { target: 'es5', targetFolder: 'src', dtsMain: 'index.d.ts' },
+                { input: 'src/index.js', moduleName: ['fesm5', 'main', 'esm5'], outputFile: 'simples.js', format: 'cjs' },
+                { target: 'es2015', input: 'es2015/index.js', moduleName: ['fesm2015', 'esm2015'], outputFile: 'simples.js', format: 'cjs' },
+                { target: 'es2017', input: 'es2017/index.js', moduleName: ['fesm2017', 'esm2017'], outputFile: 'simples.js', format: 'cjs' }
+            ]
+        }
+    ]
 })
-export class SimpleBuilder {
+export class SimpleBuilder implements AfterInit {
+    onAfterInit(): void | Promise<void> {
+        console.log('mvc build has inited...')
+    }
 }
 
 if (process.cwd() === __dirname) {
-    Workflow.create()
-        .use(PackModule)
-        .bootstrap(SimpleBuilder);
+    Workflow.run(SimpleBuilder);
 }
