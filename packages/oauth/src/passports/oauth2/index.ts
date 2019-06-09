2e19b106c969e370796c2466fbb93e59e4d012f6
houjun <freebirds8281@hotmail.com>
2019-06-09 09:17:54 +0800

    remove

--- packages/oauth/src/passports/oauth2/index.ts
+++
@@ -1,4 +0,0 @@
-export { AuthorizationError, TokenError } from './errors';
-export { BaseStateStore, SessionStore, StoreMeta } from './stateStore';
-export { BaseOAuth2Strategy } from './strategy';
-export * from './oauth2';
