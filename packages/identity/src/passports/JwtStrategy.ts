24d17ffd5d7a481c37f939d85a0e1e2041b3c663
houjun <freebirds8281@hotmail.com>
2019-07-15 01:16:04 +0800

    update

---
+++ packages/identity/src/passports/JwtStrategy.ts
@@ -0,0 +1,250 @@
+import { Strategy } from './Strategy';
+import { Context, Request, Response } from 'koa';
+import { ValidationResult, FailResult, SuccessResult } from './results';
+import { Component, Input, AfterInit } from '@tsdi/components';
+import { IStrategyOption, IContext } from '@mvx/mvc';
+import * as url from 'url';
+const jwt = require('jsonwebtoken');
+
+
+export type JwtVerify = (payload: any, ctx?: IContext) => Promise<{ user, info }>;
+
+/**
+ * JwtStrategyOption Option
+ *
+ * @export
+ * @interface JwtStrategyOption
+ * @extends {IStrategyOption}
+ */
+export interface JwtStrategyOption extends IStrategyOption {
+    secretOrKey?: string | Buffer[];
+    secretOrKeyProvider?: (request: Request, rawJwtToken) => Promise<any>;
+    jwtFromRequest: (request: Request) => any;
+    verify: JwtVerify;
+    // If defined issuer will be verified against this value
+    issuer: string;
+
+    audience: any;
+    algorithms: string[];
+    ignoreExpiration?: boolean;
+    passReqToCallback?
+}
+
+
+@Component({
+    selector: 'jwt'
+})
+export class JwtStrategy extends Strategy implements AfterInit {
+
+    @Input() protected verify: JwtVerify;
+    @Input() issuer: string;
+    @Input() audience: any;
+    @Input() algorithms: string[];
+    @Input() ignoreExpiration?: boolean;
+    @Input() secretOrKey: string | Buffer[];
+    @Input() secretOrKeyProvider: (request: Request, rawJwtToken) => Promise<any>;
+    @Input() jwtFromRequest: (request: Request) => any;
+
+    async onAfterInit(): Promise<void> {
+        if (!this.name) {
+            this.name = 'jwt';
+        }
+        if (!this.verify) {
+            throw new TypeError('JwtStrategy requires a verify');
+        }
+        if (!this.jwtFromRequest) {
+            throw new TypeError('JwtStrategy requires a function to retrieve jwt from requests (see option jwtFromRequest)');
+        }
+    }
+
+    async authenticate(ctx: Context, options?: any): Promise<ValidationResult> {
+        let token = this.jwtFromRequest(ctx.request);
+        if (!token) {
+            return new FailResult('No auth token', 401);
+        }
+        let secretOrKey = await this.secretOrKeyProvider(ctx.request, token)
+
+        let payload = await new Promise((j, r) => {
+            jwt.verify(token, secretOrKey, {
+                audience: this.audience,
+                issuer: this.issuer,
+                algorithms: this.algorithms,
+                ignoreExpiration: this.ignoreExpiration
+            }, (err, decoded) => {
+                if (err) {
+                    j(err);
+                } else {
+                    r(decoded);
+                }
+            });
+        });
+
+        let { user, info } = await this.verify(payload, ctx as IContext);
+
+        if (!user) {
+            // TODO, not sure 401 is the correct meaning
+            return new FailResult(info, 401);
+        }
+        return new SuccessResult(options, user, info);
+    }
+
+}
+
+
+
+
+function auth_header(target, varArgs) {
+    if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    to[nextKey] = nextSource[nextKey];
+                }
+            }
+        }
+    }
+    return to;
+}
+
+const matcExp = /(\S+)\s+(\S+)/;
+function parseAuthHeader(hdrValue) {
+    if (typeof hdrValue !== 'string') {
+        return null;
+    }
+    var matches = hdrValue.match(matcExp);
+    return matches && { scheme: matches[1], value: matches[2] };
+}
+
+
+// Note: express http converts all headers
+// to lower case.
+const AUTH_HEADER = 'authorization',
+    LEGACY_AUTH_SCHEME = 'JWT',
+    BEARER_AUTH_SCHEME = 'bearer';
+
+export namespace JwtHeanders {
+
+
+    export function fromHeader(headerName: string) {
+        return function (request) {
+            var token = null;
+            if (request.headers[headerName]) {
+                token = request.headers[headerName];
+            }
+            return token;
+        };
+    };
+
+
+
+    export function fromBodyField(fieldName) {
+        return function (request) {
+            var token = null;
+            if (request.body && Object.prototype.hasOwnProperty.call(request.body, fieldName)) {
+                token = request.body[fieldName];
+            }
+            return token;
+        };
+    };
+
+
+
+    export function fromUrlQueryParameter(paramName) {
+        return function (request) {
+            let token = null,
+                parsedUrl = url.parse(request.url, true);
+            if (parsedUrl.query && Object.prototype.hasOwnProperty.call(parsedUrl.query, paramName)) {
+                token = parsedUrl.query[paramName];
+            }
+            return token;
+        };
+    };
+
+
+
+    export function fromAuthHeaderWithScheme(authScheme) {
+        var authSchemeLower = authScheme.toLowerCase();
+        return function (request) {
+
+            var token = null;
+            if (request.headers[AUTH_HEADER]) {
+                var authParams = parseAuthHeader(request.headers[AUTH_HEADER]);
+                if (authParams && authSchemeLower === authParams.scheme.toLowerCase()) {
+                    token = authParams.value;
+                }
+            }
+            return token;
+        };
+    };
+
+
+
+    export function fromAuthHeaderAsBearerToken() {
+        return fromAuthHeaderWithScheme(BEARER_AUTH_SCHEME);
+    };
+
+
+    export function fromExtractors(extractors) {
+        if (!Array.isArray(extractors)) {
+            throw new TypeError('export function fromExtractors expects an array')
+        }
+
+        return function (request) {
+            var token = null;
+            var index = 0;
+            while (!token && index < length) {
+                token = extractors[index].call(this, request);
+                index++;
+            }
+            return token;
+        }
+    };
+
+
+    /**
+     * This extractor mimics the behavior of the v1.*.* extraction logic.
+     *
+     * This extractor exists only to provide an easy transition from the v1.*.* API to the v2.0.0
+     * API.
+     *
+     * This extractor first checks the auth header, if it doesn't find a token there then it checks the 
+     * specified body field and finally the url query parameters.
+     * 
+     * @param options
+     *          authScheme: Expected scheme when JWT can be found in HTTP Authorize header. Default is JWT. 
+     *          tokenBodyField: Field in request body containing token. Default is auth_token.
+     *          tokenQueryParameterName: Query parameter name containing the token. Default is auth_token.
+     */
+    export function versionOneCompatibility(options) {
+        var authScheme = options.authScheme || LEGACY_AUTH_SCHEME,
+            bodyField = options.tokenBodyField || 'auth_token',
+            queryParam = options.tokenQueryParameterName || 'auth_token';
+
+        return function (request) {
+            var authHeaderExtractor = fromAuthHeaderWithScheme(authScheme);
+            var token = authHeaderExtractor(request);
+
+            if (!token) {
+                var bodyExtractor = fromBodyField(bodyField);
+                token = bodyExtractor(request);
+            }
+
+            if (!token) {
+                var queryExtractor = fromUrlQueryParameter(queryParam);
+                token = queryExtractor(request);
+            }
+
+            return token;
+        };
+    }
+
+}
