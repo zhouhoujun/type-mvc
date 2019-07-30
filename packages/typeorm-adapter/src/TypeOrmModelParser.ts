89aac62b4e97a82561f2887575f67c41a45af6ba
houjun <freebirds8282@hotmail.com>
2019-07-30 16:55:43 +0800

    db type check

--- packages/typeorm-adapter/src/TypeOrmModelParser.ts
+++ packages/typeorm-adapter/src/TypeOrmModelParser.ts
@@ -1,9 +1,8 @@
-import { ModelParser, DefaultModelParserToken } from '@mvx/mvc';
-import { Singleton, Type, ObjectMap, PropertyMetadata, Autorun, SymbolType, Token, isFunction } from '@tsdi/ioc';
+import { ModelParser, DefaultModelParserToken, DBPropertyMetadata } from '@mvx/mvc';
+import { Singleton, Type, ObjectMap, Autorun, SymbolType, Token, isFunction, isString } from '@tsdi/ioc';
 import { getMetadataArgsStorage } from 'typeorm';
 import { ColumnMetadataArgs } from 'typeorm/metadata-args/ColumnMetadataArgs';
 import { ObjectID } from 'mongodb';
-import { isString } from 'util';
 
 @Singleton(DefaultModelParserToken)
 @Autorun('setup')
@@ -18,13 +17,14 @@ export class TypeOrmModelParser extends ModelParser {
             .register(ObjectID, (id) => new ObjectID(id));
     }
 
-    protected getPropertyMeta(type: Type): ObjectMap<PropertyMetadata[]> {
+    protected getPropertyMeta(type: Type): ObjectMap<DBPropertyMetadata[]> {
         let metas = {};
         getMetadataArgsStorage().columns.filter(col => col.target === type)
             .forEach(col => {
                 metas[col.propertyName] = metas[col.propertyName] || [];
-                metas[col.propertyName].push(<PropertyMetadata>{
+                metas[col.propertyName].push(<DBPropertyMetadata>{
                     propertyKey: col.propertyName,
+                    dbtype: isString(col.options.type) ? col.options.type : '',
                     type: this.getModeType(col)
                 });
             });
@@ -33,7 +33,7 @@ export class TypeOrmModelParser extends ModelParser {
             .forEach(col => {
                 metas[col.propertyName] = metas[col.propertyName] || [];
                 let relaModel = isFunction(col.type) ? col.type() as Token : undefined;
-                metas[col.propertyName].push(<PropertyMetadata>{
+                metas[col.propertyName].push(<DBPropertyMetadata>{
                     propertyKey: col.propertyName,
                     provider: relaModel,
                     type: (col.relationType === 'one-to-many' || col.relationType === 'many-to-many') ? Array : relaModel
@@ -42,20 +42,50 @@ export class TypeOrmModelParser extends ModelParser {
         return metas;
     }
 
+    protected isExtendBaseType(type: SymbolType, propmeta?: DBPropertyMetadata): boolean {
+        if (propmeta.dbtype) {
+            if (/int/.test(propmeta.dbtype)) {
+                return true;
+            }
+        }
+        if (type === ObjectID) {
+            return true;
+        }
+        return super.isExtendBaseType(type, propmeta);
+    }
+
+    protected resolveExtendType(type: SymbolType, value: any, propmeta?: DBPropertyMetadata): any {
+        if (propmeta.dbtype) {
+            if (/int/.test(propmeta.dbtype)) {
+                return parseInt(value);
+            }
+        }
+        if (type === ObjectID) {
+            return new ObjectID(value);
+        }
+        return super.resolveExtendType(type, propmeta);
+    }
+
+
+
     protected getModeType(col: ColumnMetadataArgs) {
         let type: SymbolType = col.options.type;
         if (type) {
             if (isString(type)) {
                 if (type === 'uuid') {
                     return String;
-                } else if (/(int|float|double|dec)/.test(type)) {
+                } else if (/(int|float|double|dec|numeric|number)/.test(type)) {
                     return Number;
-                } else if (/bool/.test(type)) {
+                } else if (/(bool|bit)/.test(type)) {
                     return Boolean;
-                } else if (/(var|string|text)/.test(type)) {
+                } else if (/(char|var|string|text)/.test(type)) {
                     return String;
-                } else if (/time/.test(type)) {
+                } else if (/(time|date)/.test(type)) {
                     return Date;
+                } else if (/array/.test(type)) {
+                    return Array;
+                } else if (/(bytes|bytea)/.test(type)) {
+                    return Buffer
                 } else {
                     return Object;
                 }
