c367c0be765529f5d825b8b9c4a62f26f0372d6c
houjun <freebirds8281@hotmail.com>
2020-04-12 21:51:46 +0800

    update dep demo

--- packages/cli/temp/src/controllers/UsersController.ts
+++ packages/cli/temp/src/controllers/UsersController.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Post, Cors, Put, Authorization, Delete, IContext, ContextToken } from '@mvx/mvc';
 import { User } from '../models/User';
-import { UserService } from '../services/UserService';
 import { Inject } from '@tsdi/ioc';
 import { ResponseResult } from '../ResponseResult';
+import { UserRepository } from '../repositories/UserRepository';
 
 @Cors
 @Authorization('admin')
@@ -12,45 +12,45 @@ export class UserController {
     @Inject(ContextToken)
     context: IContext;
 
-    constructor(private service: UserService) {
+    constructor(private repos: UserRepository) {
     }
 
     // @Post('/')
     // async add(user: User) {
-    //     let r = await this.service.save(user);
+    //     let r = await this.repos.save(user);
     //     return ResponseResult.success(r);
     // }
 
     // @Put('/')
     // async middfy(user: User) {
-    //     let r = await this.service.save(user);
+    //     let r = await this.repos.save(user);
     //     return ResponseResult.success(r);
     // }
 
     @Put('/')
     @Post('/')
     async save(user: User) {
-        let r = await this.service.save(user);
+        let r = await this.repos.save(user);
         return ResponseResult.success(r);
     }
 
 
     @Get('/:id')
     async get(id: string) {
-        let user = this.service.findById(id);
+        let user = this.repos.findById(id);
         return ResponseResult.success(user);
     }
 
     @Delete('/:id')
     async remove(id: string) {
-        let user = await this.service.removeById(id);
+        let user = await this.repos.removeById(id);
         return ResponseResult.success(user);
     }
 
     @Get('/')
     async query(keywords: string, skip?: number, take?: number) {
-        let query = await this.service.query(keywords, skip, take);
-        return ResponseResult.success(query[0], query[1]);
+        const [ users, total ] = await this.repos.search(keywords, skip, take);
+        return ResponseResult.success(users, total);
     }
 
 }
--- packages/cli/temp/src/services/UserService.ts
+++ packages/cli/temp/src/services/UserService.ts
@@ -1,20 +1,18 @@
 import { Inject, Singleton } from '@tsdi/ioc';
-import { TypeOrmHelper } from '@tsdi/typeorm-adapter';
 import { User } from '../models/User';
+import { UserRepository } from '../repositories/UserRepository';
 
 @Singleton()
 export class UserService {
 
-    @Inject()
-    db: TypeOrmHelper;
+    @Inject() private userRep: UserRepository;
 
     constructor() {
 
     }
 
     async verify(username: string, password: string) {
-        let userRep = await this.db.getRepository(User);
-        let user = await userRep.findOne({ where: { account: username } });
+        let user = await this.userRep.findOne({ where: { account: username } });
         if (!user || user.password !== password) {
             return { user: null, info: false };
         }
@@ -22,53 +20,8 @@ export class UserService {
     }
 
     async verifyJWT(id: string) {
-        let userRep = await this.db.getRepository(User);
-        let user = await userRep.findOne(id);
+        let user = await this.userRep.findOne(id);
         return user;
-    }
-
-    async save(user: User) {
-        let userRep = await this.db.getRepository(User);
-        return await userRep.save(user);
-    }
-
-    async findById(id: string) {
-        let rep = await this.db.getRepository(User);
-        return await rep.findOne(id);
-    }
-
-    async findByAccount(name: string) {
-        let rep = await this.db.getRepository(User);
-        return await rep.findOne({ where: { account: name } });
-    }
-
-    async removeById(id: string) {
-        let rep = await this.db.getRepository(User);
-        let user = await rep.findOne(id);
-        return await rep.remove(user);
-    }
-
-    async remove(user: User) {
-        let rep = await this.db.getRepository(User);
-        return await rep.remove(user);
-    }
-
-    async query(keywords?: string, skip?: number, take?: number): Promise<[User[], number]> {
-        let rep = await this.db.getRepository(User);
-        let sqb = rep.createQueryBuilder('user')
-            .leftJoinAndSelect('user.role', 'role');
-        if (keywords) {
-            sqb = sqb
-                .where('user.name LIKE :param')
-                .orWhere('user.account LIKE :param')
-                .orWhere('user.email LIKE :param')
-                .orWhere('user.phone LIKE :param')
-                .setParameter('param', `%${keywords}%`)
-        }
-        return await sqb.orderBy('user.name', 'ASC')
-            .skip(skip)
-            .take(take)
-            .getManyAndCount();
     }
 
 }
